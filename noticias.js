3cdb341e1c1bfed444fe9d9772e639aea0c02d6e
root <root@matheus.matheussuffi>
2018-07-09 22:41:22 -0300

    first commit

---
+++ noticias.js
@@ -0,0 +1,14 @@
+var http = require('http');
+http.createServer(function (req, res){
+
+    var categoria = req.url;
+
+
+    if(categoria == '/tecnologia'){
+        res.end('<html><body>Noticias de tecnologia</body></html>');   
+    }else if (categoria == '/moda'){
+        res.end('<html><body>Noticias de moda</body></html>');
+    }else{
+        res.end('<html><body>Portal de noticias</body></html>');
+    }
+}).listen(3000); 
